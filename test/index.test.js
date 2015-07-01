c6a1ebc8a70c7f20f53a2ab91077d8f3f9468d24
Nathan Houle <nathan@nathanhoule.com>
2015-06-30 18:59:30 -0700

    Release 1.0.4

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 
-var Analytics = require('analytics.js').constructor;
+var Analytics = require('analytics.js-core').constructor;
 var integration = require('analytics.js-integration');
 var tester = require('analytics.js-integration-tester');
 var AdWords = require('../lib/');
