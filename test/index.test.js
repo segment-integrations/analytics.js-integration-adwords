5aa01dc5f4aab19b28669377d61c047b03afb867
Han <hankim813@gmail.com>
2016-09-16 18:28:48 -0700

    Revert "Dynamic remarketing"

--- test/index.test.js
+++ test/index.test.js
@@ -73,37 +73,7 @@ describe('AdWords', function() {
         analytics.page();
         analytics.called(window.google_trackConversion, {
           google_conversion_id: options.conversionId,
-          google_custom_params: {
-            path: window.location.pathname,
-            referrer: document.referrer,
-            search: '',
-            title: '',
-            url: window.location.href
-          },
-          google_remarketing_only: true
-        });
-      });
-
-      it('should load remarketing with page props and window.google_custom_params', function() {
-        adwords.options.remarketing = true;
-        window.google_custom_params = {
-          dynx_itemid: ['1'],
-          dynx_pagetype: 'offerdetail',
-          dynx_totalvalue: '42'
-        };
-        analytics.page();
-        analytics.called(window.google_trackConversion, {
-          google_conversion_id: options.conversionId,
-          google_custom_params: {
-            path: window.location.pathname,
-            referrer: document.referrer,
-            search: '',
-            title: '',
-            url: window.location.href,
-            dynx_itemid: ['1'],
-            dynx_pagetype: 'offerdetail',
-            dynx_totalvalue: '42'
-          },
+          google_custom_params: {},
           google_remarketing_only: true
         });
       });
@@ -112,7 +82,6 @@ describe('AdWords', function() {
     describe('#track', function() {
       beforeEach(function() {
         analytics.stub(window, 'google_trackConversion');
-        delete window.google_custom_params;
       });
 
       it('should not send if event is not defined', function() {
@@ -163,43 +132,18 @@ describe('AdWords', function() {
         });
       });
 
-      it('should support remarketing if enabled', function() {
+      it('should always send remarketing_only false', function() {
         adwords.options.remarketing = true;
-        analytics.track('login', { revenue: 90, custom: 'summer sixteen' });
+        analytics.track('login', { revenue: 90 });
         analytics.called(window.google_trackConversion, {
           google_conversion_id: options.conversionId,
-          google_custom_params: { custom: 'summer sixteen' },
+          google_custom_params: {},
           google_conversion_language: 'en',
           google_conversion_format: '3',
           google_conversion_color: 'ffffff',
           google_conversion_label: options.events.login,
           google_conversion_value: 90,
-          google_remarketing_only: true
-        });
-      });
-
-      it('should send custom params from window with properties for remarketing', function() {
-        window.google_custom_params = {
-          dynx_itemid: ['1'],
-          dynx_pagetype: 'offerdetail',
-          dynx_totalvalue: '42'
-        };
-        adwords.options.remarketing = true;
-        analytics.track('login', { revenue: 90, custom: 'summer sixteen' });
-        analytics.called(window.google_trackConversion, {
-          google_conversion_id: options.conversionId,
-          google_custom_params: { 
-            custom: 'summer sixteen',
-            dynx_itemid: ['1'],
-            dynx_pagetype: 'offerdetail',
-            dynx_totalvalue: '42'
-          },
-          google_conversion_language: 'en',
-          google_conversion_format: '3',
-          google_conversion_color: 'ffffff',
-          google_conversion_label: options.events.login,
-          google_conversion_value: 90,
-          google_remarketing_only: true
+          google_remarketing_only: false
         });
       });
     });
