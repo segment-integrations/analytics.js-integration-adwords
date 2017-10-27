766fbb2e826e1b19e91278229186eea9bb568e57
Justin Lee <justinlee@Justins-MacBook-Pro.local>
2017-10-27 08:42:57 -0700

    merged options

--- test/index.test.js
+++ test/index.test.js
@@ -11,29 +11,36 @@ describe('AdWords', function() {
   var analytics;
   var options = {
     conversionId: 978352801,
-    labelMap: [
+    eventMappings: [
       {
         key: 'signup',
         value: {
           eventName: 'signup',
-          label: '-kGkCJ_TsgcQofXB0gM'
+          label: '-kGkCJ_TsgcQofXB0gM',
+          conversionId: '',
+          remarketing: false
         }
       },
       {
         key: 'login',
         value: {
           eventName: 'login',
-          label: 'QbThCM_zogcQofXB0gM'
+          label: 'QbThCM_zogcQofXB0gM',
+          conversionId: '',
+          remarketing: false
         }
       },
       {
         key: 'play',
         value: {
           eventName: 'play',
-          label: 'b91fc77f'
+          label: 'b91fc77f',
+          conversionId: '',
+          remarketing: false
         }
       }
-    ]
+    ],
+    pageRemarketing: false
   };
 
   beforeEach(function() {
@@ -54,9 +61,8 @@ describe('AdWords', function() {
   it('should have the correct settings', function() {
     analytics.compare(AdWords, integration('AdWords')
       .option('conversionId', '')
-      .option('remarketing', false)
-      .option('whitelist', [])
-      .option('labelMap', []));
+      .option('pageRemarketing', false)
+      .option('eventMappings', []));
   });
 
   describe('loading', function() {
@@ -78,7 +84,7 @@ describe('AdWords', function() {
       });
 
       it('should not load remarketing if option is not on', function() {
-        adwords.options.remarketing = false;
+        adwords.options.pageRemarketing = false;
         analytics.page();
         analytics.calledOnce(window.google_trackConversion);
         analytics.deepEqual(window.google_trackConversion.args[0], [{
@@ -89,7 +95,7 @@ describe('AdWords', function() {
       });
 
       it('should fire additional remarketing tag if option is on', function() {
-        adwords.options.remarketing = true;
+        adwords.options.pageRemarketing = true;
         analytics.page();
         analytics.calledTwice(window.google_trackConversion);
         // fire conversion tag first
@@ -131,14 +137,14 @@ describe('AdWords', function() {
           google_conversion_language: 'en',
           google_conversion_format: '3',
           google_conversion_color: 'ffffff',
-          google_conversion_label: options.labelMap[0].value.label,
+          google_conversion_label: options.eventMappings[0].value.label,
           google_conversion_value: 0,
           google_remarketing_only: false
         });
       });
 
       it('should use mapping specific IDs if provided', function() {
-        adwords.options.labelMap = [
+        adwords.options.eventMappings = [
           {
             key: 'signup',
             value: {
@@ -148,19 +154,11 @@ describe('AdWords', function() {
             }
           },
           {
-            key: 'login',
+            key: 'signup',
             value: {
-              eventName: 'login',
+              eventName: 'signup',
               label: 'QbThCM_zogcQofXB0gM',
               conversionId: 23
-            }
-          },
-          {
-            key: 'play',
-            value: {
-              eventName: 'play',
-              label: 'b91fc77f',
-              conversionId: 4512
             }
           }
         ];
@@ -171,14 +169,24 @@ describe('AdWords', function() {
           google_conversion_language: 'en',
           google_conversion_format: '3',
           google_conversion_color: 'ffffff',
-          google_conversion_label: options.labelMap[0].value.label,
+          google_conversion_label: options.eventMappings[0].value.label,
+          google_conversion_value: 0,
+          google_remarketing_only: false
+        });
+        analytics.called(window.google_trackConversion, {
+          google_conversion_id: 23,
+          google_custom_params: {},
+          google_conversion_language: 'en',
+          google_conversion_format: '3',
+          google_conversion_color: 'ffffff',
+          google_conversion_label: options.eventMappings[1].value.label,
           google_conversion_value: 0,
           google_remarketing_only: false
         });
       });
 
-      it('should use whitelisted event-specific ID when provided', function() {
-        adwords.options.whitelist = [{ value: { eventName: 'danny mcbride is funny', conversionIds: [4879235] } }];
+      it('should send remarketing along with conversion if both are enabled', function() {
+        adwords.options.eventMappings = [{ value: { eventName: 'danny mcbride is funny', conversionId: 4879235, remarketing: true, label: '' } }];
         analytics.track('danny mcbride is funny', { revenue: 90 });
         analytics.calledOnce(window.google_trackConversion);
         analytics.deepEqual(window.google_trackConversion.args[0], [{
@@ -196,7 +204,7 @@ describe('AdWords', function() {
           google_conversion_language: 'en',
           google_conversion_format: '3',
           google_conversion_color: 'ffffff',
-          google_conversion_label: options.labelMap[1].value.label,
+          google_conversion_label: options.eventMappings[1].value.label,
           google_conversion_value: 90,
           google_remarketing_only: false
         });
@@ -211,7 +219,7 @@ describe('AdWords', function() {
           google_conversion_language: 'en',
           google_conversion_format: '3',
           google_conversion_color: 'ffffff',
-          google_conversion_label: options.labelMap[1].value.label,
+          google_conversion_label: options.eventMappings[1].value.label,
           google_conversion_value: 90,
           google_remarketing_only: false
         });
@@ -227,14 +235,14 @@ describe('AdWords', function() {
           google_conversion_language: 'en',
           google_conversion_format: '3',
           google_conversion_color: 'ffffff',
-          google_conversion_label: options.labelMap[1].value.label,
+          google_conversion_label: options.eventMappings[1].value.label,
           google_conversion_value: 90,
           google_remarketing_only: false
         }]);
       });
 
       it('should send only the remarketing tag if no conversions are mapped but is whitelisted', function() {
-        adwords.options.whitelist = [{ value: { eventName: 'danny mcbride is funny', conversionIds: [] } }];
+        adwords.options.eventMappings = [{ value: { eventName: 'danny mcbride is funny', conversionId: '', remarketing: true, label: '' } }];
         analytics.track('danny mcbride is funny', { revenue: 90 });
         analytics.calledOnce(window.google_trackConversion);
         analytics.deepEqual(window.google_trackConversion.args[0], [{
@@ -245,7 +253,7 @@ describe('AdWords', function() {
       });
 
       it('should send both conversion and remarketing tag if remarketing is true', function() {
-        adwords.options.remarketing = true;
+        adwords.options.eventMappings[1].value.remarketing = true;
         analytics.track('login', { revenue: 90, hello: 'foo' });
         analytics.calledTwice(window.google_trackConversion);
         analytics.deepEqual(window.google_trackConversion.args[0], [{
@@ -254,7 +262,7 @@ describe('AdWords', function() {
           google_conversion_language: 'en',
           google_conversion_format: '3',
           google_conversion_color: 'ffffff',
-          google_conversion_label: options.labelMap[1].value.label,
+          google_conversion_label: options.eventMappings[1].value.label,
           google_conversion_value: 90,
           google_remarketing_only: false
         }]);
@@ -263,13 +271,6 @@ describe('AdWords', function() {
           google_custom_params: { hello: 'foo' },
           google_remarketing_only: true
         }]);
-      });
-
-      it('should not double send remarketing tag as a standalone if it was already sent with conversion tag', function() {
-        adwords.options.remarketing = true;
-        analytics.track('login', { revenue: 90, hello: 'foo' });
-        // It would be called three times if it sent duplicate
-        analytics.calledTwice(window.google_trackConversion);
       });
     });
   });
